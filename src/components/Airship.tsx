e523b69d7db1df0bbd3a5699ab66f568354de470
wwei-github <418047394@qq.com>
2022-07-03 22:45:31 +0800

    旋转飞船方向

--- src/components/Airship.tsx
+++ src/components/Airship.tsx
@@ -1,20 +1,42 @@
-import {useEffect,useRef,Suspense} from "react";
-import { useGLTF,useAnimations } from "@react-three/drei";
+import React, { useEffect, useRef, Suspense, useImperativeHandle } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
 import * as THREE from "three";
 
-const Airship = () => {
+interface shipModelRef {
+  group: React.RefObject<THREE.Group>;
+}
+
+const AirshipModel = React.forwardRef<shipModelRef>((props, ref) => {
   const group = useRef<THREE.Group>(null);
   const { scene, animations } = useGLTF("/models/bull_dog/scene.gltf");
-  const { names, actions } = useAnimations(animations,group);
+  const { names, actions } = useAnimations(animations, group);
   useEffect(() => {
     actions[names[0]]?.play();
   });
+  useImperativeHandle(ref, () => ({
+    group,
+  }));
+
   return (
     <>
-    <group ref={group}>
-      <primitive object={scene} />
-    </group>
+      <group ref={group}>
+        <primitive object={scene} scale={3}/>
+      </group>
+    </>
+  );
+});
+
+const Airship = () => {
+  const AirshipModelRef = useRef<shipModelRef>(null);
+  useEffect(() => {
+    if (!AirshipModelRef.current) return;
+    AirshipModelRef.current.group.current!.rotation.set(0, Math.PI, 0);
+  }, []);
+  return (
+    <>
+      <AirshipModel ref={AirshipModelRef} />
     </>
   );
 };
+
 export default Airship;
